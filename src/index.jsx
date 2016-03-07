b8f9c19ed8a61e0cafe7f777825d09e08ed9a98c
Darren Bortel <drbortel@gmail.com>
2016-03-07 13:36:46 -0600

    fixed index into dist, add router for App and voting results

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
+
+console.log(document.getElementById('app'));
+
 ReactDOM.render(
-    <Voting pair={pair} />,
-    document.getElementById('app')
+  <Router history={hashHistory}>{routes}</Router>,
+  document.getElementById('app')
 );
