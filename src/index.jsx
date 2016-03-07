91c2e18b81705d54fa246ee3315ecdea17ca5175
Darren Bortel <drbortel@gmail.com>
2016-03-07 16:10:33 -0600

    added reducer, store, provider, app, containers for Voting and Results

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
 import App from './components/App';
-import Voting from './components/Voting';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
 import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
+
 const routes = <Route component={App}>
-  <Route path="/results" component={Results} />
-  <Route path="/" component={Voting} />
+  <Route path="/results" component={ResultsContainer} />
+  <Route path="/" component={VotingContainer} />
 </Route>;
 
 console.log(document.getElementById('app'));
 
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
