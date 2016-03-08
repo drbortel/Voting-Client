8baa6d111c2ae8adeda8bac5a1ed72946648a9d7
Darren Bortel <drbortel@gmail.com>
2016-03-07 23:50:59 -0600

    added middleware with next button action creator and meta remote tag

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import io from 'socket.io-client';
 import reducer from './reducer';
 import {setState} from './action_creators';
+import remoteActionMiddleware from './remote_action_middleware';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
 import Results from './components/Results';
 
-const pair = ['Trainspotting', '28 Days Later'];
-
-const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
-
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
   store.dispatch(setState(state))
 );
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
